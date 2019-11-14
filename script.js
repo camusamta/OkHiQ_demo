4fc8cbb1e0bb1a11f4ad252dbfed23ef02211bb9
Sam <progfan@gmail.com>
2019-11-15 09:22:42 +1000

    UI update

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
